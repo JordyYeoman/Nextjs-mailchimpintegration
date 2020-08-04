b36c522d28193368722b48e370ef34d6113c9112
JordyYeoman <jordyyeoman@gmail.com>
2020-08-04 13:45:27 +0800

    First commit to Nextjs-mailchimp repo

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,48 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { useState } from 'react';
 
-export default function Home() {
+const Home = () => {
+
+  const [input, setInput] = useState('');
+
+  const subscribe = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await fetch('./api/subscribe', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          emailAddress: input
+        })
+      })
+
+      if (res.status === 200) {
+        alert('You are subscribed!')
+      } else {
+        alert('Sorry, something went wrong.')
+      }
+    } catch (err) {
+      alert(err)
+    }
+  }
+
   return (
-    <div className={styles.container}>
+    <div className='p-8 justify-center items-center h-screen flex'>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <form className='flex'>
+        <input value={input}
+          onChange={e => setInput(e.target.value)} className='bg-gray-200 shadow-inner rounded-l p-2 flex-1 w-64' id='email' type='email' aria-label='email address' placeholder='Enter your email address' />
+        <button onClick={subscribe} className='bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r' type='submit'>
+          Sign Up
+              </button>
+      </form>
     </div>
   )
 }
+
+export default Home
